39e8895bd33f38226ff0233b11d02b007026d2f1
ManiDeepak <manideepak371@gmail.com>
2022-06-23 21:20:51 +0530

    cors issue resolve

--- main.js
+++ main.js
@@ -21,8 +21,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"*",
-    credentials:true
+    origin:"*"
 }))
 
 // app.use('/',(req,res)=>{
