77d02cc8fd6ad6f4d45fd3b8756527910ce5b37b
ManiDeepak <manideepak371@gmail.com>
2022-06-23 20:27:35 +0530

    cors issue check

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://tovisit-india-web.netlify.app/",
     credentials:true
 }))
 
