19979e0d1719d814087a0c2e6fc91a1c9b281174
ManiDeepak <manideepak371@gmail.com>
2022-06-24 20:14:17 +0530

    cors issue resolve

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://tovisit-india-web.netlify.app"
+    origin:"*"
 }))
 
 // app.use('/',(req,res)=>{
